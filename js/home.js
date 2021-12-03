8aeae9288ff14c68b4ab2c748bd90828402813c3
9908229348 <kalavaisravanthi98@gmail.com>
2021-12-03 12:05:29 +0530

    Home page-Using Template Literals

--- js/home.js
+++ js/home.js
@@ -3,21 +3,13 @@ window.addEventListener('DOMContentLoaded', (event) => {
 });
 
 const createInnerHtml = () => {
-    const innerHtml =   `
-        <tr>
-            <th></th>
-            <th>Name</th>
-            <th>Gender</th>
-            <th>Department</th>
-            <th>Salary</th>
-            <th>Start Date</th>
-            <th>Actions</th>
-        </tr>
+    const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th>";
+    const innerHtml =   `${headerHtml}
         <tr>
         <td><img class="profile" alt="" src="../assets/profile-images/Ellipse -2.png"</td>
         <td>Narayan Mahadevan</td>
         <td>Male</td>
-        <td><div class="dept-label">Engineering</div>
+        <td><div class="dept-label">HR</div>
             <div class="dept-label">Finance</div></td>
         <td>300000</td>
         <td>1 Nov 2020</td>
